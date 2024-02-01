66c230d3029a670e14939c75ec5984afc53ed3aa
lvsx <lvsx@t-chip.com.cn>
2024-02-01 09:39:55 +0800

    fix the crash problem caused by high temperature VPU fixed frequency

--- drivers/video/rockchip/mpp/mpp_rkvenc.c
+++ drivers/video/rockchip/mpp/mpp_rkvenc.c
@@ -949,9 +949,10 @@ static struct devfreq_governor devfreq_venc_ondemand = {
 static unsigned long rkvenc_get_static_power(struct devfreq *devfreq,
 					     unsigned long voltage)
 {
-	struct rkvenc_dev *enc = devfreq->data;
+	struct device *dev = devfreq->dev.parent;
+	struct rkvenc_dev *enc = dev_get_drvdata(dev);
 
-	if (!enc->model_data)
+	if (!enc || !enc->model_data)
 		return 0;
 	else
 		return rockchip_ipa_get_static_power(enc->model_data,
