a58b0e483cc0b4b70d1421acee794dd0970b27dd
daijh <djh@t-chip.com.cn>
2024-08-27 18:20:50 +0800

    driver: pc9202_wdt: use private data

--- drivers/watchdog/pc9202_wdt.c
+++ drivers/watchdog/pc9202_wdt.c
@@ -47,35 +47,36 @@
 #define WDT_KICK_S_40_96			0xf8
 #define WDT_KICK_DISABLED			(0x00)
 #define PC9202_VERSION		"firefly 1.0.0   04/15/2023"
-bool iWriteByte(uint8_t addr, uint8_t data);
 
-static unsigned char demoBuffer[16];
-
-struct sw2001 {
+struct pc9202_pdata {
+	int major;
+	struct class *cls;
+	struct device *dev;
+	struct gpio_desc *wd_en_gpio;
 	struct i2c_client	*client;
 	struct mutex		lock;
-	//struct platform_device	*sw2001_device;
+	unsigned char demoBuffer[16];
 };
 
-static struct sw2001 *the_sw2001;
-static int major;
-static struct class *cls;
-static struct device *dev;
-// int wd_en_gpio;
-struct gpio_desc *wd_en_gpio = NULL;
+struct pc9202_device {
+	struct cdev cdev;
+	struct pc9202_pdata * data;
+};
 
-void enable_wdt(void)
+bool iWriteByte(uint8_t addr, uint8_t data, struct pc9202_pdata *pdata);
+
+void enable_wdt(struct gpio_desc * gpio, struct pc9202_pdata *pdata)
 {
 	printk("====== enabled 9202 wdt ======\n");
-	gpiod_direction_output(wd_en_gpio, 1);
+	gpiod_direction_output(gpio, 1);
 }
 
-void disable_wdt(void)
+void disable_wdt(struct gpio_desc * gpio, struct pc9202_pdata *pdata)
 {
 	printk("====== disabled 9202 wdt ======\n");
-	gpiod_direction_output(wd_en_gpio, 0);
+	gpiod_direction_output(gpio, 0);
 	msleep(10);
-	iWriteByte(SW2001_REG_WDT_CTRL,WDT_KICK_DISABLED);
+	iWriteByte(SW2001_REG_WDT_CTRL,WDT_KICK_DISABLED, pdata);
 }
 
 /*-------------------------------------------------------------------------*/
@@ -83,19 +84,19 @@ void disable_wdt(void)
  * reg:
  * data:
  */
-int sw2001_write(unsigned char reg, unsigned char *buf, int len)
+int sw2001_write(unsigned char reg, unsigned char *buf, int len , struct pc9202_pdata *pdata)
 {
 	int	 status=0;
 
-	if (!the_sw2001)
+	if (!pdata)
 		return -ENODEV;
 
-	mutex_lock(&the_sw2001->lock);
+	mutex_lock(&pdata->lock);
 
-	//status = i2c_smbus_write_byte_data(the_sw2001->client, reg, data);
-	status = i2c_smbus_write_i2c_block_data(the_sw2001->client, reg, len, buf);
+	//status = i2c_smbus_write_byte_data(pdata->client, reg, data);
+	status = i2c_smbus_write_i2c_block_data(pdata->client, reg, len, buf);
 
-	mutex_unlock(&the_sw2001->lock);
+	mutex_unlock(&pdata->lock);
 
 	//printk("%s: sw2001_write(0x%x,0x%x)\n", "pc9202", reg, *buf);
 
@@ -116,26 +117,26 @@ int sw2001_write(unsigned char reg, unsigned char *buf, int len)
 //               TRUE : success
 //               FALSE : fail
 //*********************************************************************************
-bool iWriteByte(uint8_t addr, uint8_t data)
+bool iWriteByte(uint8_t addr, uint8_t data , struct pc9202_pdata *pdata)
 {
 	//sw2001_write(addr, &data, 1);
-	return sw2001_write(addr, &data, 1);
+	return sw2001_write(addr, &data, 1 , pdata);
 }
 
 /* sw2001_read parameter:
  * reg:
  * data:
  */
-int sw2001_read(unsigned char reg, unsigned char *buf, int len)
+int sw2001_read(unsigned char reg, unsigned char *buf, int len, struct pc9202_pdata *pdata)
 {
 	int	 status=0;
 
-	if (!the_sw2001)
+	if (!pdata)
 		return -ENODEV;
-	mutex_lock(&the_sw2001->lock);
-	//status = i2c_smbus_read_byte_data(the_sw2001->client, reg);
-	status = i2c_smbus_read_i2c_block_data(the_sw2001->client, reg, len, buf);
-	mutex_unlock(&the_sw2001->lock);
+	mutex_lock(&pdata->lock);
+	//status = i2c_smbus_read_byte_data(pdata->client, reg);
+	status = i2c_smbus_read_i2c_block_data(pdata->client, reg, len, buf);
+	mutex_unlock(&pdata->lock);
 	printk("%s: sw2001_read(0x%x) return 0x%x\n", "pc9202", reg, status);
 
 	printk("status=0x%x\n",status);
@@ -145,20 +146,10 @@ int sw2001_read(unsigned char reg, unsigned char *buf, int len)
 }
 EXPORT_SYMBOL(sw2001_read);
 
-//*********************************************************************************
-// function    : iReadByte(uint8_t addr, uint8_t data)
-// description :  read data from addr(internal register address in device)
-// parameters  :
-//                  addr : register address in device
-//                  data:  data
-// return        :
-//               TRUE : success
-//               FALSE : fail
-//*********************************************************************************
-bool iReadByte(uint8_t addr, uint8_t *data)
+bool iReadByte(uint8_t addr, uint8_t *data ,struct pc9202_pdata *pdata)
 {
 	//sw2001_read(addr, data, 1);
-	return sw2001_read(addr, data, 1);
+	return sw2001_read(addr, data, 1, pdata);
 }
 
 static int pc9202_wdt_suspend(struct device *dev)
@@ -173,6 +164,8 @@ static int pc9202_wdt_resume(struct device *dev)
 
 int wdt_open(struct inode *inode, struct file *filp)
 {
+	struct pc9202_device *dev = container_of(inode->i_cdev, struct pc9202_device, cdev);
+	filp->private_data = dev->data;
 	//printk("wdt_open \r\n");
     return 0;
 }
@@ -183,16 +176,6 @@ int wdt_release(struct inode *inode, struct file *filp)
 }
 ssize_t wdt_read(struct file *filp, char __user *buf, size_t count,loff_t *f_pos)
 {
-#if 0
-	uint8_t reg_value;
-	iReadByte(SW2001_REG_WDT_CTRL, &reg_value);
-	/* 把数据复制到应用程序空间 */
-    if (copy_to_user(buf,&reg_value,1))
-    {
-		count=-EFAULT;
-	}
-#endif
-	//printk("wdt_read \r\n");
 	return count;
 }
 
@@ -200,26 +183,27 @@ ssize_t wdt_write(struct file *filp, const char __user *buf, size_t count,loff_t
 {
 	/* 把数据复制到内核空间 */
 	uint8_t len = (int)count;
+	struct pc9202_pdata *data = filp->private_data;
 	if(len>2)
 	{
 		printk("wdt:inviald value too long\r\n");
 		return count;
 	}
-	if (copy_from_user(demoBuffer+*f_pos, buf, count))
+	if (copy_from_user(data->demoBuffer+*f_pos, buf, count))
 	{
 		count = -EFAULT;
     }
-	switch(demoBuffer[0])
+	switch(data->demoBuffer[0])
 	{
-		case '0':iWriteByte(SW2001_REG_WDT_CTRL,WDT_KICK_S_0_64);break;//0.64
-		case '1':iWriteByte(SW2001_REG_WDT_CTRL,WDT_KICK_S_2_56);break;//2.56
-		case '2':iWriteByte(SW2001_REG_WDT_CTRL,WDT_KICK_S_10_24);break;//10.24
-		case '3':iWriteByte(SW2001_REG_WDT_CTRL,WDT_KICK_S_40_96);break;//40.96
-		case 'e':enable_wdt();break; //enable wdt
-		case 'd':disable_wdt();break; //disable wdt
+		case '0':iWriteByte(SW2001_REG_WDT_CTRL,WDT_KICK_S_0_64, data);break;//0.64
+		case '1':iWriteByte(SW2001_REG_WDT_CTRL,WDT_KICK_S_2_56, data);break;//2.56
+		case '2':iWriteByte(SW2001_REG_WDT_CTRL,WDT_KICK_S_10_24, data);break;//10.24
+		case '3':iWriteByte(SW2001_REG_WDT_CTRL,WDT_KICK_S_40_96, data);break;//40.96
+		case 'e':enable_wdt(data->wd_en_gpio, data);break; //enable wdt
+		case 'd':disable_wdt(data->wd_en_gpio, data);break; //disable wdt
 		default:printk("wdt:inviald value \r\n");break;
 	}
-	//printk("wdt_weite %c\r\n", demoBuffer[0]);
+	//printk("wdt_weite %c\r\n", data->demoBuffer[0]);
 	return count;
 }
 
@@ -232,89 +216,105 @@ struct file_operations wdt_fops = {
 };
 
 static int pc9202_wdt_probe(struct i2c_client *client,
-        const struct i2c_device_id *id)
+		const struct i2c_device_id *id)
 {
 
 	struct device_node *node = client->dev.of_node;
-	struct sw2001	*pEnc;
+	struct pc9202_device	*dev;
+	struct pc9202_pdata	*data;
 	uint8_t reg_value;
 	int retry_count, ret;
+	dev_t devno;
 	const char * driver_name =  "wdt_crl";
 
 	dev_info(&client->dev, "Version: %s\n", PC9202_VERSION);
 
-    if (!of_device_is_available(client->dev.of_node)) {
+	if (!of_device_is_available(client->dev.of_node)) {
 		return 0;
-    }
+	}
 
-    if (!i2c_check_functionality(client->adapter, I2C_FUNC_I2C)) {
-        dev_err(&client->dev, "I2C functionality not supported\n");
-        return -ENODEV;
-    }
+	if (!i2c_check_functionality(client->adapter, I2C_FUNC_I2C)) {
+		dev_err(&client->dev, "I2C functionality not supported\n");
+		return -ENODEV;
+	}
 
-	pEnc = kzalloc(sizeof(struct sw2001), GFP_KERNEL);
-	if (!pEnc)
+	dev = kzalloc(sizeof(struct pc9202_device), GFP_KERNEL);
+	if (!dev)
+		return -ENOMEM;
+	data = kzalloc(sizeof(struct pc9202_pdata), GFP_KERNEL);
+	if (!data)
 		return -ENOMEM;
 
-	mutex_init(&pEnc->lock);
-	pEnc->client = client;
-	i2c_set_clientdata(client,pEnc);
-	the_sw2001 = pEnc;
+	dev->data = data;
+
+	mutex_init(&data->lock);
+	data->client = client;
+	
+	i2c_set_clientdata(client,dev);
 
 	for (retry_count = 0; retry_count < 100; retry_count++) {
-	    ret = iReadByte(SW2001_REG_WDT_CTRL, &reg_value);
-	    if(0 != ret) {
-		dev_err(dev,"====== i2c detect failed watchdog init err: 0x%x ======\n", reg_value);
-	    } else {
-		dev_err(dev,"====== i2c detect success watchdog init ======\n");
-		break;
-	    }
-	    msleep(10);
+		ret = iReadByte(SW2001_REG_WDT_CTRL, &reg_value, data);
+		if(0 != ret) {
+			dev_err(data->dev,"====== i2c detect failed watchdog init err: 0x%x ======\n", reg_value);
+		} else {
+			dev_err(data->dev,"====== i2c detect success watchdog init ======\n");
+			break;
+		}
+		msleep(10);
 	}
 
 	if (0 != ret) {
-	    dev_err(dev,"====== i2c detect failed watchdog init ======\n");
-	    goto err;
+		dev_err(data->dev,"====== i2c detect failed watchdog init ======\n");
+		goto err;
 	}
 
 	ret = of_property_read_string(node, "driver-names", &driver_name);
-	major = register_chrdev(0, driver_name, &wdt_fops);
-    if (major < 0)
-    {
-		dev_err(dev,"Unable to register wdt character device !\n");
-		goto err;
-    }
-	// 创建类
-	cls = class_create(THIS_MODULE, driver_name);
-	// 创建设备节点
-	dev = device_create(cls, NULL, MKDEV(major, 0), NULL, driver_name);
+	
+	// register chrdev
+	alloc_chrdev_region(&devno, 0, 1, driver_name);
+	
+	data->major = MAJOR(devno);
+	
+	cdev_init(&dev->cdev, &wdt_fops);
 
-	wd_en_gpio = devm_gpiod_get_optional(&client->dev, "wd-en", GPIOD_ASIS);
-
-	if (IS_ERR(wd_en_gpio)) {
-		ret = PTR_ERR(wd_en_gpio);
-		if (ret != -EPROBE_DEFER)
-			dev_err(dev, "failed to get reset GPIO: %d\n", ret);
+	ret = cdev_add(&dev->cdev, devno, 1);
+	if (ret < 0) {
+		unregister_chrdev_region(devno, 1);
+		printk(KERN_WARNING "cdev_add failed\n");
 		return ret;
 	}
 
-    return 0;
+	// 创建类
+	data->cls = class_create(THIS_MODULE, driver_name);
+	// 创建设备节点
+	data->dev = device_create(data->cls, NULL, MKDEV(data->major, 0), NULL, driver_name);
+
+	data->wd_en_gpio = devm_gpiod_get_optional(&client->dev, "wd-en", GPIOD_ASIS);
+
+	if (IS_ERR(data->wd_en_gpio)) {
+		ret = PTR_ERR(data->wd_en_gpio);
+		if (ret != -EPROBE_DEFER)
+			dev_err(data->dev, "failed to get reset GPIO: %d\n", ret);
+		return ret;
+	}
+
+	return 0;
 err:
-	kfree(pEnc);
+	kfree(data);
+	kfree(dev);
 	i2c_set_clientdata(client, NULL);
-	the_sw2001 = NULL;
 	return 0;
 }
 
 static int pc9202_wdt_remove(struct i2c_client *client)
 {
-	struct sw2001		*axp = i2c_get_clientdata(client);
+	struct pc9202_device    *dev = i2c_get_clientdata(client);
 
 	//PR_DEBUG("%s\n",__func__);
 
-	kfree(axp);
+	kfree(dev->data);
+	kfree(dev);
 	i2c_set_clientdata(client, NULL);
-	the_sw2001 = NULL;
     return 0;
 }
 
@@ -355,9 +355,6 @@ static int __init pc9202_wdt_init(void)
 }
 static void __exit pc9202_wdt_exit(void)
 {
-	if (!IS_ERR(wd_en_gpio) && wd_en_gpio!= NULL) {
-		disable_wdt();
-	}
     i2c_del_driver(&pc9202_wdt_driver);
     return;
 }
